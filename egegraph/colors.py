7ca42095919cd80f4cd8973e24804bad22f05d2f
malmsteen <philipenich@list.ru>
2024-06-24 22:58:31 +0300

    Added file with Monokai colors and pastel colors

---
+++ egegraph/colors.py
@@ -0,0 +1,20 @@
+
+pastel_background = "#2e2e2e"
+pastel_comments = "#797979"
+pastel_white = "#d6d6d6" "#ffd76d"
+pastel_yellow = "#e5b567"
+pastel_green = "#b4d273"
+pastel_orange = "#e87d3e"
+pastel_purple = "#9e86c8"
+pastel_pink = "#b05279"
+pastel_blue = "#6c99bb"
+
+MONOKAI_PINK = '#f92672'
+MONOKAI_ORANGE = '#fd971f'
+MONOKAI_YELLOW = '#e6da74'
+MONOKAI_BLUE = '#66d9ef'
+MONOKAI_GREEN = '#a6e22e'
+MONOKAI_PURPLE = '#ae81ff'
+MONOKAI_GRAY = '#272822'
+MONOKAI_LIGHTGRAY = '#74715e'
+MONOKAI_WHITE = '#f6f6f6'
